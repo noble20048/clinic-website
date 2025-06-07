91044ce4ca9607a5566087be5156d1c2a91eec25
noble20048 <noblebijukattungal@gmail.com>
2025-06-07 22:49:22 +0530

    initial commit for clinic website

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.H>
+int main()
+{
+read a,b;
+printf("enter value a:");
+printf("enter value b:");
+scanf("%d/n");
+}
+{
+int sum()
+sum=a+b;
+printf("the value is:");
+scanf("%d\n");
+}
+return 0;
